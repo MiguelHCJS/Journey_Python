fdbb3e22e051955ee478e6d0e2198596b7a06c65
Miguel Tenório <69335370+MiguelHCJS@users.noreply.github.com>
2022-10-14 13:57:11 -0300

    Update exercícios_estruturaDeRepetição.py

--- exercicios/estrutura de repetição/exercícios_estruturaDeRepetição.py
+++ exercicios/estrutura de repetição/exercícios_estruturaDeRepetição.py
@@ -1,5 +1,6 @@
-# -- QUESTÃO 1 --
-# 1. Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+''' -- QUESTÃO 1 --
+  Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+'''
 def questao1():
   while True:
     try:
@@ -13,8 +14,9 @@ def questao1():
       print('Valor digitado, inválido. Tente novamente, digite uma nota.')
 
 
-# -- QUESTÃO 2 --
-# 2. Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+''' -- QUESTÃO 2 --
+  Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+'''
 def questao2():
   while True:
     usuario = input('Login: ')
@@ -26,24 +28,125 @@ def questao2():
       print('Digite uma senha maior que 6 digitos e diferente do usuário.')
 
 
-"""
-3. Faça um programa que leia e valide as seguintes informações:
+""" -- QUESTÃO 3 --
+  Faça um programa que leia e valide as seguintes informações:
     1. Nome: maior que 3 caracteres;
     2. Idade: entre 0 e 150;
     3. Salário: maior que zero;
     4. Sexo: 'f' ou 'm';
     5. Estado Civil: 's', 'c', 'v', 'd';
 """
+def solicitar_nome():
+  while True:
+    nome = input('Qual é o seu nome? ')
+    if len(nome) >= 3:
+      break
+    elif len(nome) < 3:
+      print('Digite um nome maior que 3 caracteres.')
+  return nome
 
+
+def solicitar_idade():
+  while True:
+    try:
+      idade = int(input('Digite sua idade: '))
+      break
+    except:
+      print('Digite a idade em número.')
+  return idade
+
+
+def solicitar_salario():
+  while True:
+    try:
+      salario = float(input('Digite o salário: '))
+      if salario >= 0:
+        break
+      else:
+        print('Digite um salário, 0 ou mais.')
+    except:
+      print('Digite um valor válido.')
+  return salario
+
+
+def solicitar_sexo():
+  sexo_selecionado = None
+
+  while sexo_selecionado == None:
+    sexo = input('Selecione o sexo, [M]asculino [F]eminino: ').lower()
+    if sexo == 'm':
+      sexo_selecionado = 'Masculino'
+      break
+    elif sexo == 'f':
+      sexo_selecionado = 'Feminino'
+      break
+    elif sexo != 'm' or sexo != 'm':
+      print('Selecione um opção válida, [M] ou [F].)')
+  return sexo_selecionado
+
+
+def solicitar_estado_civil():
+  estado_civil_selecionado = None
+
+  while estado_civil_selecionado == None:
+    estado_civil = input('''
+    Selecione um estado civil:
+    [S]olteiro,
+    [C]asado,
+    [V]iuvo,
+    [D]ivorciado.
+    ''').lower()
+    if estado_civil == 's':
+      estado_civil = 'Solteiro(a)'
+      break
+    elif estado_civil == 'c':
+      estado_civil = 'Casado(a)'
+      break
+    elif estado_civil == 'v':
+      estado_civil = 'Viuvo(a)'
+      break
+    elif estado_civil == 'd':
+      estado_civil = 'Divorciado(a)'
+      break
+    else:
+      print('Selecione um opção válida.')
+  return estado_civil
+
+
+def questao3():
+  print(f'''
+        Seu nome é {solicitar_nome()}
+        Sexo {solicitar_sexo()}
+        Você tem {solicitar_idade()} anos
+        Ganha R$ {solicitar_salario()}
+        E é {solicitar_estado_civil()}''')
+
+""" -- QUESTÃO 4 --
+  Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
 """
-4. Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
-"""
+paisA = 80000
+paisB = 200000
 
-# 5. Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
+contador_de_anos = 0
 
-# 6. Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+while paisA <= paisB:
+  paisA *= 1.03
+  paisB *= 1.015
+  contador_de_anos += 1
 
-# 7. Faça um programa que leia 5 números e informe o maior número.
+print(f'Pais A tem {round(paisA * 1.03)} e Pais B {round(paisB * 1.015)}, levou {contador_de_anos} anos para se igualar.')
+
+''' -- QUESTÃO 5 --
+  Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
+'''
+
+''' -- QUESTÃO 6 --
+  Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+'''
+
+''' -- QUESTÃO 7 --
+Faça um programa que leia 5 números e informe o maior número.
+'''
 
 # 8. Faça um programa que leia 5 números e informe a soma e a média dos números.
 
